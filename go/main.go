514288b989c6c74561ee9f3fb917bbf26b57b4db
KingSeojee <30362922+seojeenyeok@users.noreply.github.com>
2018-05-02 23:17:55 +0900

    디렉토리 구조 재정의

---
+++ go/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Recover())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "Hello world")
+	})
+	e.Logger.Fatal(e.Start(":8081"))
+}
