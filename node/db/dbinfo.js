6c6a2f286eb759aee9a8f3fafb1ea2f9b040168f
PTHy <30455428+PTHy@users.noreply.github.com>
2018-05-04 23:33:17 +0900

    change dbinfo
    오류가 있었습니다

--- node/db/dbinfo.js
+++ node/db/dbinfo.js
@@ -1,5 +1,8 @@
+module.exports = (function (){
   return {
     local: {
+      host : 'localhost',
+      port : '3306',
       user : 'root',
       password : 'rkxdlrkcl',
       database : 'undefined'
