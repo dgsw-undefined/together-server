c216585b63a8d146f327687477828d7b2e030d02
PTHy <30455428+PTHy@users.noreply.github.com>
2018-05-12 16:42:32 +0900

    주석 추가, TeamCreate 조금
    TeamCreate 마무리 해야돼요!!

--- node/routes/team/team.controller.js
+++ node/routes/team/team.controller.js
@@ -1,10 +1,13 @@
+//Protocol Format 연결
+const protocol = require('../../util/protocolFormat')
+//JWT 모듈 encoding : Hs256
+const jwt = require('jsonwebtoken')
+//Mysql 접속
 const mysql_dbc = require('../../db/dbcon')();
 const mysql = require('mysql')
 const pool = mysql_dbc.init();
-const protocol = require('../../util/protocolFormat')
-const jwt = require('jsonwebtoken')
-// encoding : Hs256
 mysql_dbc.test_open(pool);
+//query 명령어
 var stmt = null;
 
 /*
@@ -29,6 +32,13 @@ exports.list = (req, res) => {
   Post /team
 */
 
+//todo Team Create 해야돼!!
 exports.create = (req, res) => {
-  stmt = 'INSERT INTO team (name,subject,desc,leader_id) values ('
+  stmt = 'INSERT INTO team (name,subject,desc,leader_id) values (?,?,?,?)'
+  params = [req.body.name,req.body.subject,req.body.desc,req.body.leader_id]
+  pool.getConnection((err,connection) => {
+    connection.query(stmt,params,(err,rows) => {
+      if(err) return protocol.error(err)
+    });
+  });
 }
