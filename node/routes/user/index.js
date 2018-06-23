a7ba44d104f77ea2f4048b5da9292ddb1348439d
KingSeojee <30362922+seojeenyeok@users.noreply.github.com>
2018-06-23 14:11:57 +0900

    [UPDATE] GET/user/:mode

--- node/routes/user/index.js
+++ node/routes/user/index.js
@@ -5,5 +5,6 @@ router.patch('/able', controller.available)
 router.get('/trust',controller.truster_list)
 router.post('/trust',controller.trust)
 router.post('/untrust',controller.untrust)
-
+router.get('/user/:mode')
+  
 module.exports = router
--- node/routes/user/user.controller.js
+++ node/routes/user/user.controller.js
@@ -1,5 +1,5 @@
 //Protocol Format 연결
-const protocol = require('../../util/protocolFormat').trust
+const protocol = require('../../util/protocolFormat')
 //JWT 모듈 encoding : Hs256
 const jwt = require('jsonwebtoken')
 //Mysql 접속
@@ -30,10 +30,10 @@ exports.available = (req,res) => {
 */
 
 exports.truster_list = (req,res) => {
-  stmt = 'SELECT * FROM truster WHERE user_id = '+parseInt(req.decoded.iss)
+  stmt = 'SELECT * FROM truster WHERE user_id = '+mysql.escape(req.decoded.name)
   pool.getConnection((err,connection) => {
     connection.query(stmt,(err,rows) => {
-      if(err) protocol.error(res,err)
+      if(err) protocol.error(res.err)
       if(rows == 0) protocol.notFound(res)
       protocol.success(res,rows)
     });
@@ -47,13 +47,15 @@ exports.truster_list = (req,res) => {
 
 exports.trust = (req, res) => {
   stmt = 'INSERT INTO truster (user_id,truster_id) VALUES (?,?)'
-  var params = [parseInt(req.decoded.iss),mysql.escape(req.body.trust_id)]
+  var params = [req.decoded.name,req.body.trust_id]
 
-  pool.getConnection((err,connection) => {
-    connection.query(stmt,params,(err,rows) => {
+  pool.getConnection((err, connection) => {
+    connection.query(stmt,params,(err, rows) => {
       if(err) protocol.error(res,err)
+      console.log("decoded name : "+req.decoded.name+" req.body.trust_id : "+req.body.trust_id)
       protocol.success(res)
-    })
+    });
+    connection.release()
   })
 }
 
@@ -63,11 +65,54 @@ exports.trust = (req, res) => {
 
 exports.untrust = (req, res) => {
   stmt = 'DELETE FROM truster WHERE user_id = ? AND truster_id = ?'
-  params = [parseInt(req.decoded.iss),mysql.escape(req.body.trust_id)]
+  params = [mysql.escape(req.decoded.name),mysql.escape(req.body.trust_id)]
   pool.getConnection((err,connection) => {
     connection.query(stmt,params,(err,rows) => {
       if(err) protocol.error(err)
       protocol.success(res)
     })
   })
+}
+
+exports.userList=(req,res)=>{
+  let mode=req.params.mode;
+  const date_Asending=()=>{
+    stmt='SELECT name,enroll_date FROM user order by enroll_date';
+    pool.getConnection((err,connection) => {
+      connection.query(stmt,(err,rows) => {
+        if(err) protocol.list.error(res.err)
+        protocol.list.success(res,rows)
+      });
+      connection.release()
+    })
+  }
+  const date_Desending=()=>{
+    stmt='SELECT name,enroll_date FROM user order by enroll_date DESC';
+    pool.getConnection((err,connection) => {
+      connection.query(stmt,(err,rows) => {
+        if(err) protocol.list.error(res.err)
+        protocol.list.success(res,rows)
+      });
+      connection.release()
+    })
+  }
+  const truster_Desending=()=>{
+    stmt='SELECT *,count(CASE WHEN user.idx=truster.truster_id THEN 1 END) as trustercount FROM user join truster on user.idx=truster.truster_id group by idx order by trustercount DESC';
+    pool.getConnection((err,connection) => {
+      connection.query(stmt,(err,rows) => {
+        if(err) protocol.list.error(res.err)
+        protocol.list.success(res,rows)
+      });
+      connection.release()
+    })
+  }
+  if(mode==1){//가입 날짜 오름차순
+    date_Asending();
+  }else if(mode==2){//가입 날짜 내림차순
+    date_Desending();
+  }else if(mode==3){//트러스터 수 내림차순
+    truster_Desending()
+  }else{
+    protocol.list.badRequset()
+  }
 }
